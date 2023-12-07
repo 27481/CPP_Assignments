06bcc0ec6a393e1ab05e74e112cc8523fc08f02c
27481 <utkarshpandey519@gmail.com>
2023-12-07 19:49:44 +0530

    This is merging

--- 01_13_Mar_Intro_To_CPP/02_Assignment/01_Q1.cpp
+++ 01_13_Mar_Intro_To_CPP/02_Assignment/01_Q1.cpp
@@ -2,22 +2,20 @@
 typedef unsigned long long int ll;
 using namespace std;
 
-int main() {
+int main()
+{
 
-int x;
+    int x;
 
-cout << "Enter first number\n";
-cin >> x; // user will give ‘x’ a value.
+    cout<<"Enter first number\n";
+    cin>>x; // user will give ‘x’ a value.
 
-int y, m;
-cout << "Enter second number and value for taking modulus\n";
-cin >> y >> m; // user will give ‘y’ a value.
+    int y, m;
+    cout<<"Enter second number and value for taking modulus\n";
+    cin>>y>>m; // user will give ‘y’ a value.
 
-int Z = (x * y) % m;
-cout << "Output is: " << Z;
+    int Z=(x*y)%m;
+    cout << "Output is: "<<Z;
 
-    
-          
- 
     return 0;
 }
